2f00bd525857c4038dfeee1e3496a55a50b7ab4a
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-03-12 12:10:37 -0700

    Update order.js

--- models/order.js
+++ models/order.js
@@ -29,6 +29,7 @@ Order.findByStatus = (status) => {
             'id', U.id,
             'name', U.name,
             'lastname', U.lastname,
+            'phone', U.phone,
             'image', U.image
         ) AS client,
 		JSON_BUILD_OBJECT(
@@ -102,6 +103,7 @@ Order.findByDeliveryAndStatus = (id_delivery, status) => {
             'id', U.id,
             'name', U.name,
             'lastname', U.lastname,
+            'phone', U.phone,
             'image', U.image
         ) AS client,
 		JSON_BUILD_OBJECT(
@@ -177,7 +179,8 @@ Order.findByClientAndStatus = (id_client, status) => {
             'id', U.id,
             'name', U.name,
             'lastname', U.lastname,
-            'image', U.image
+            'image', U.image,
+            'phone', U.phone,
         ) AS client,
 		JSON_BUILD_OBJECT(
             'id', U2.id,
