970d409f63783410df7b4260a0e4bf0d860b9b66
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-03-21 14:46:53 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -14,7 +14,7 @@ types.setTypeParser(1114, function (stringValue) {
 
 const databaseConfig = {
     user: 'djomewgzclmvfm',
-    host: 'ec2-3-93-160-246.compute-1.amazonaws.com',
+    host: 'ec2-52-5-167-89.compute-1.amazonaws.com',
     database: 'd2p8e37i44vdqm',
     password:'2b1374a525bfdec543198b1a250f44e95097aa45ccf441c18525aaa991ad6278',
     port: 5432,
