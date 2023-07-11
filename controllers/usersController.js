a01d16fcc62bbb2218746cc8e5c2fa4b13238996
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-07-11 07:22:08 -0700

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -353,6 +353,68 @@ module.exports = {
             });
         }
     },
+
+     async login(req, res, next) {
+        try {
+            const email = req.body.id;
+            const password = req.body.password;
+ 
+            const myUser = await User.findByEmail2(id);
+ 
+            if (!myUser) {
+                return res.status(401).json({
+                    success: false,
+                    message: 'El email no fue encontrado'
+                });
+            }
+ 
+            if (User.isPasswordMatched2(password, myUser.password)) {
+                const token = jwt.sign({id: myUser.id, email: myUser.email}, keys.secretOrKey, {
+                    // expiresIn: (60*60*24) // 1 HORA
+                   //  expiresIn: (60 * 2) // 2 MINUTOS
+                });
+                const data = {
+                    id: myUser.id,
+                    name: myUser.name,
+                    lastname: myUser.lastname,
+                    email: myUser.email,
+                    phone: myUser.phone,
+                    image: myUser.image,
+                    session_token: `JWT ${token}`, // REVISA QUE ESTE LLEGANDO ESTE CAMPO
+                    autenticated: myUser.autenticated,
+                    is_trainer: myUser.is_trainer,
+                    document: myUser.document,
+                    roles: myUser.roles
+                }
+
+                await User.updateToken(myUser.id, `JWT ${token}`);
+                
+ 
+                console.log(`DATA ENVIADA ${data.roles}`); // AQUI PUEDES VER QUE DATOS ESTAS ENVIANDO
+ 
+                return res.status(201).json({
+                    success: true,
+                    data: data,
+                    message: '! BIENVENIDO !'
+                });
+            }
+            else {
+                return res.status(401).json({
+                    success: false,
+                    message: 'La contraseña es incorrecta'
+                });
+            }
+ 
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Error al momento de hacer login',
+                error: error
+            });
+        }
+    },
  
    async updateNotificationToken(req, res, next) {
         try {
