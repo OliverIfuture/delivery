0fbe057d8b6fda9b3bf3c6fdd806225608cc55e4
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-08-04 18:20:10 -0700

    Update order.js

--- models/order.js
+++ models/order.js
@@ -346,11 +346,9 @@ sales(
     is_trainer,
     image_client,
     reference,
-    hour,
-    quantity
-
+    hour
         )
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11,$12) RETURNING id
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11) RETURNING id
     `;
 
     return db.oneOrNone(sql, [
