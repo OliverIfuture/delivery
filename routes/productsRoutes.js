ceab1d353f8a4657ab95a169e52b60bf555588ed
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-08-08 11:28:56 -0700

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,8 +4,7 @@ const productsControllers = require('../controllers/productsControllers');
 module.exports = (app, upload) => {
       app.post('/api/products/create',passport.authenticate('jwt', {session: false}) , upload.array('image', 3), productsControllers.create);
       app.post('/api/products/createTab',passport.authenticate('jwt', {session: false}) , upload.array('image', 3), productsControllers.createTab);
-      
-      app.get('/api/products/getAll', productsControllers.getAll);
+      app.get('/api/products/getAll',passport.authenticate('jwt', {session: false}) , productsControllers.getAll);
 
       app.get('/api/products/findByCategory/:id_category',passport.authenticate('jwt', {session: false}) , productsControllers.findByCategory);
       app.get('/api/products/findByCategoryAndProductName/:id_category/:product_name', passport.authenticate('jwt', {session: false}), productsControllers.findByCategoryAndProductName);
