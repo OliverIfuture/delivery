be1855fd6a6a14d9842c9ce7f7ff942c5c185c0e
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-08-17 08:00:16 -0700

    Update ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -411,7 +411,26 @@ async createSale(req, res, next) {
             });
         }
     },
-        
+     async selectOpenShiftExpenses(req, res, next) {
+        try {
+            const date = req.params.date;
+
+            const data = await Order.selectOpenShiftExpenses(date);
+            console.log(`Status: ${JSON.stringify(data)}`);
+            return res.status(201).json(data);
+            
+        } catch (error) {
+            
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                message: 'Hubo un error al tratar de obtener las ventas',
+                error: error,
+                success: false
+            
+
+            });
+        }
+    },    
 }
 
 
