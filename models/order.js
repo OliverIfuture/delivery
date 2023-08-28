0b62c2f728340fceb602eb8da010329f31a7fac6
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-08-28 10:56:12 -0700

    Update order.js

--- models/order.js
+++ models/order.js
@@ -416,7 +416,48 @@ Order.selectOrder = (date, shift_ref ) => {
 
 
 
+Order.ShiftOrders = (shift_ref ) => {
+    const sql = `
+ SELECT 
+        S.id,
+        S.name_store,
+        S.cash,
+        S.credit_card,
+        S.points,
+        S.date,
+        S.total,
+        S.employed,
+        S.is_trainer,
+        S.image_client,
+        S.reference,	
+	S.hour,
+        S.shift_ref,
+        JSON_AGG(
+            JSON_BUILD_OBJECT(
+                'id', P.id,
+                'product_name', P.product_name,
+                'product_price', P.product_price,
+                'image_product', P.image_product,
+		'product_coast', P.product_coast,
+                'reference', P.reference,
+		'quantity', P.quantity
+            )
+        ) AS productsOrder
 
+    FROM 
+        sales AS S
+    INNER JOIN
+        order_sales AS P
+    ON
+        P.reference = S.reference where S.shift_ref = $1
+       GROUP BY
+        S.id
+	ORDER BY S.id ASC
+    `;
+    return db.manyOrNone(sql, [
+			  shift_ref
+    			]);
+}
 
 Order.closeShift = (sales) => {
     const sql = `
