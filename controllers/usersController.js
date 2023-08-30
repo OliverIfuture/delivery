e01e1628bbd6a37149a011b429e35534716a18e4
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-08-29 19:16:46 -0700

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -48,7 +48,26 @@ module.exports = {
             });
         }
     },
+    async findByMail(req, res, next) {
+        try {
 
+            const email = req.params.email;
+
+            const data = await User.findByMail(email);
+            console.log(`Datos enviados del usuario: ${JSON.stringify(data)}`);
+            return res.status(201).json(data);
+
+
+        }
+        catch (error) {
+            
+            console.log(`error: ${error}`);
+            return res.status(501).json({
+                succes: false,
+                message: 'error al obtener el usuario por ID'
+            });
+        }
+    },
      async getShops(req, res, next) {
         try {
 
