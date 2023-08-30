fa59ad9dba3e49fb34d84ba9f2fe807d47f8f849
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-08-29 17:14:41 -0700

    Update ordersRoutes.js

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -11,6 +11,7 @@ module.exports = (app) => {
     app.get('/api/orders/findByClientAndStatus/:id_client/:status', passport.authenticate('jwt', {session: false}), ordersController.findByClientAndStatus);
         
     app.get('/api/orders/ShiftOrders/:shift_ref', ordersController.ShiftOrders);
+    app.get('/api/orders/selectOrderAll/:date', ordersController.selectOrderAll);
 
     app.get('/api/orders/selectOrder/:date/:shift_ref', ordersController.selectOrder);
     app.get('/api/orders/selectOpenShift', ordersController.selectOpenShift);
