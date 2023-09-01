f67a66450f42582b83d7be3d6ec3b033fd9e4873
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-08-31 19:57:13 -0700

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -5,7 +5,7 @@ module.exports = (app, upload) => {
       app.post('/api/products/create',passport.authenticate('jwt', {session: false}) , upload.array('image', 3), productsControllers.create);
       app.post('/api/products/createTab',passport.authenticate('jwt', {session: false}) , upload.array('image', 3), productsControllers.createTab);
       app.get('/api/products/getAll',passport.authenticate('jwt', {session: false}) , productsControllers.getAll);     
-      app.get('/api/products/getAllStocks/:id_company ',passport.authenticate('jwt', {session: false}) , productsControllers.getAllStocks );
+      app.get('/api/products/getAllStocks/:id_company',passport.authenticate('jwt', {session: false}) , productsControllers.getAllStocks);
 
       app.post('/api/products/setStock', passport.authenticate('jwt', { session: false }), productsControllers.setStock);
 
@@ -13,7 +13,7 @@ module.exports = (app, upload) => {
       app.get('/api/products/findByCategoryStocks/:id_category/:id_company',passport.authenticate('jwt', {session: false}) , productsControllers.findByCategoryStocks);
 
       app.get('/api/products/findByCategoryAndProductName/:id_category/:product_name', passport.authenticate('jwt', {session: false}), productsControllers.findByCategoryAndProductName);
-      app.get('/api/products/findByCategoryAndProductNameStocks/:id_category/:product_name/:id_company', passport.authenticate('jwt', {session: false}), productsControllers.findByCategoryAndProductNameStocks );
+      app.get('/api/products/findByCategoryAndProductNameStocks/:id_category/:product_name/:id_company', passport.authenticate('jwt', {session: false}), productsControllers.findByCategoryAndProductNameStocks);
 
       app.put('/api/products/updateStock', productsControllers.updateProduct);
       app.put('/api/products/update', productsControllers.upateProduct);
