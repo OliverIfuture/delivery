51ef6ceabe59dc792c160a8110efb5ce9f738905
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-09-13 16:43:12 -0700

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -19,7 +19,7 @@ module.exports = (app, upload) => {
 
       app.put('/api/products/updateStock', productsControllers.updateProduct);
       app.put('/api/products/update', productsControllers.upateProduct);
-      app.put('/api/products/updateAdmin ', productsControllers.updateAdmin);
+      app.put('/api/products/updateAdmin', productsControllers.updateAdmin);
 
       app.put('/api/products/updateStockers/:id_product/:stock/:id_company', productsControllers.updateStockers);
 
