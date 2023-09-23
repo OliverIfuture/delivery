19e5dd93379d8a4cef5eae60d509d0f55fcb1e52
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-09-22 18:36:38 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -351,7 +351,7 @@ User.getUsersMultiNotificationTokens = () => {
     const sql = `
      SELECT         
      notification_token 
-     from users where id = 3
+     from users
     `
     return db.manyOrNone(sql);
 }
