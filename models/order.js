4faefb4d039802509548a927c231bc827dfe195e
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-10-20 16:18:09 -0700

    Update order.js

--- models/order.js
+++ models/order.js
@@ -164,6 +164,89 @@ Order.findByDeliveryAndStatus = (id_delivery, status) => {
 
 }
 
+
+Order.findByClient = (id_client) => {
+
+    const sql = `
+    SELECT 
+        O.id,
+        O.id_client,
+        O.id_address,
+        O.id_delivery,
+        O.status,
+        O.timestamp,
+	O.payMethod,
+	O.hour_program,
+        O.lat,
+        O.lng,
+	O.discounts,
+        JSON_AGG(
+            JSON_BUILD_OBJECT(
+                'id', P.id,
+                'name', P.name,
+                'description', P.description,
+                'price', P.price,
+		'price_special', P.price_special,
+                'image1', P.image1,
+                'image2', P.image2,
+                'image3', P.image3,
+                'quantity', OHP.quantity
+            )
+        ) AS products,
+        JSON_BUILD_OBJECT(
+            'id', U.id,
+            'name', U.name,
+            'lastname', U.lastname,
+            'phone', U.phone,
+	    'is_trainer', U.is_trainer,
+            'image', U.image
+        ) AS client,
+		JSON_BUILD_OBJECT(
+            'id', U2.id,
+            'name', U2.name,
+            'lastname', U2.lastname,
+            'phone', U2.phone,
+            'image', U2.image
+        ) AS delivery,
+        JSON_BUILD_OBJECT(
+            'id', A.id,
+            'address', A.address,
+            'neighborhood', A.neighborhood,
+            'lat', A.lat,
+            'lng', A.lng
+        ) AS address
+    FROM 
+        orders AS O
+    INNER JOIN
+        users AS U
+    ON
+        O.id_client = U.id
+	LEFT JOIN
+		users AS U2
+	ON
+		O.id_delivery = U2.id
+    INNER JOIN
+        address AS A
+    ON
+        A.id = O.id_address
+    INNER JOIN
+        order_has_products AS OHP
+    ON
+        OHP.id_order = O.id
+    INNER JOIN
+        products AS P
+    ON
+        P.id = OHP.id_product
+    WHERE
+        O.id_client = $1
+    GROUP BY
+        O.id, U.id, A.id, U2.id 
+    `;
+
+    return db.manyOrNone(sql, [id_client]);
+
+}
+
 Order.findByClientAndStatus = (id_client, status) => {
 
     const sql = `
