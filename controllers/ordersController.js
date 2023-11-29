bfd66356de863843b9c33a2e90043d6d583cbb2a
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2023-11-28 18:57:55 -0800

    Update ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -271,7 +271,7 @@ async createSale(req, res, next) {
             ////recorrer todos los productos de la orden
             for (const product of sales.products) {
                 console.log(`aqui entran los productos ${JSON.stringify(sales)}`);
-                await OrderHasProducts.createSale(product.name, product.price_special, product.image1, product.price_buy, sales.reference, product.quantity, sales.shift_ref);
+                await OrderHasProducts.createSale(product.name, product.price, product.image1, product.price_buy, sales.reference, product.quantity, sales.shift_ref);
 
 
             }
