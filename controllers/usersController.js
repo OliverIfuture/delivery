cb7cf11e0d94e3b146d526708b7d630ff33a62cf
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-01-02 17:33:43 -0800

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -714,10 +714,7 @@ async deleteAccout(req, res, next) {
 
     async findByCode(req, res, next) {
         try {
-            console.log(`aqui vamossss`);
             const codes = req.params.code;
-            console.log(`codigo: ${code}`);
-
             const code = await User.findByCode(codes);
 
             console.log(`codigo: ${code}`);
