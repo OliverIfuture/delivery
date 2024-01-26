f48acb4e94bb701e0bce54d862c5a870f540022b
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-01-26 12:47:20 -0800

    Update productsControllers.js

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -82,7 +82,38 @@ async setStock(req, res, next) {
         }
     },
 
+ async getSaves(req, res, next) {
+        try {
+            const id_plate = req.params.id_plate;
+            const id_user = req.params.id_user;     
+            const code = await Product.getSaves(id_plate, id_user);
+             console.log(`stock enviada: ${code}`);
 
+            if (!code) {
+                return res.status(401).json({
+                    success: false,
+                    message: 'El codigo no fue encontrado'
+                });
+            }
+
+            else{
+                return res.status(201).json({
+                success: true,
+                message: 'codigo aplicado',
+            });
+            }
+
+ 
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Error al momento de hacer login',
+                error: error
+            });
+        }
+    },
  async findSaves(req, res, next) {
         try {
             const id_plate = req.params.id_plate;
