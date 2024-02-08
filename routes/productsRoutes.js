511ad6555d7e35dea415e11b53d960144c46abfb
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-02-07 21:26:10 -0800

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -9,7 +9,7 @@ module.exports = (app, upload) => {
       app.post('/api/products/createReview',passport.authenticate('jwt', {session: false}) , productsControllers.createReview);
       app.post('/api/products/createLike/:id_plate/:username/:useremail/:id_user' , productsControllers.createLike);
       app.post('/api/products/createLikeAnswer/:id_answer/:username/:useremail/:id_user' , productsControllers.createLikeAnswer);
-      app.post('/api/products/createAnswer/:id_review/:username/:answer/:responseto/:id_user/:miUser' , productsControllers.createAnswer);
+      app.post('/api/products/createAnswer/:id_review/:username/:answer/:responseto/:id_user' , productsControllers.createAnswer);
 
       app.get('/api/products/getAll',passport.authenticate('jwt', {session: false}) , productsControllers.getAll);     
       app.get('/api/products/findLast5', productsControllers.findLast5);    
