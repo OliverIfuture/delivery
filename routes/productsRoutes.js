b7a37969ebf267cca0d7487fef1e75fae9b56536
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-02-09 20:03:15 -0800

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -65,6 +65,8 @@ module.exports = (app, upload) => {
       app.delete('/api/products/deleteSale/:id', passport.authenticate('jwt', { session: false }), productsControllers.deleteSale);
       app.delete('/api/products/deleteFavorites/:id_plate/:id_user', productsControllers.deleteFavorites);
       app.delete('/api/products/deleteFavoritesProfile/:id_profile/:id_user', productsControllers.deleteFavoritesProfile);
+      app.delete('/api/products/deleteFollowersProfile/:id_profile/:id_user', productsControllers.deleteFollowersProfile);
+
       app.delete('/api/products/deleteteSave/:id_plate/:id_user', productsControllers.deleteteSave);
 
 
