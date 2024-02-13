d03b7ebd083257d91d4d55fd7a5f116f170b9bc4
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-02-12 16:20:13 -0800

    Update product.js

--- models/product.js
+++ models/product.js
@@ -17,7 +17,7 @@ Product.createPost = (id_user, description, url) => {
 
     const sql = `
     INSERT INTO
-        users(
+        post(
             id_user,
             description,
             url
