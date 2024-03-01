b024bb3d97013e32ab320f3eca1066e8c238c585
oliverIfuture <oliverjdm2@gmail.com>
2024-02-29 18:25:43 -0800

    p

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -11,7 +11,7 @@ module.exports = {
 
 
         async createClienteOxxo(req, res, next) {
-                customer = conekta.Custoer.create({
+                customer = conekta.Customer.create({
                     "name": "Fulanito",
                     "email": "fulanito@test.com",
                     "phone": "+5218181818181",
