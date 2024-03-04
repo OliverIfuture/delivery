c1fcb4cc8ac38d8ed6542c4b42302184649a05cf
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-03-04 12:04:23 -0800

    Update ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -284,11 +284,9 @@ module.exports = {
             for (const product of order.products) {
                 if (product.id < 1000) {
                     await OrderHasProducts.create(data.id, product.id, product.quantity);
-                    await OrderHasProducts.createOrderWithPlate(data.id, product.id, product.quantity);
                     
                 }
                 if (product.id > 1000) {
-                    await OrderHasProducts.create(data.id, product.id, product.quantity);                    
                     await OrderHasProducts.createOrderWithPlate(data.id, product.id, product.quantity);
                 }
             }
