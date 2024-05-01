cd56c3d22eed1cfa1f0fbaea187fe6a1b287dc05
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-04-30 17:03:41 -0700

    Update order.js

--- models/order.js
+++ models/order.js
@@ -570,7 +570,7 @@ Order.create = (order) => {
 
 
         )
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id
     `;
 
     return db.oneOrNone(sql, [
