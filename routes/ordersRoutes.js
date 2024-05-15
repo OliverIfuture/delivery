b8aee668bb61554e422f6fc4721f3476ed776111
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-05-14 19:32:42 -0700

    Update ordersRoutes.js

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -40,6 +40,7 @@ module.exports = (app) => {
     /* 
  *put ROUTES actualizar
 */
+    app.put('/api/orders/updateCode/:id/:code', passport.authenticate('jwt', { session: false }), ordersController.updateCode);
     app.put('/api/orders/updateToDespatched', passport.authenticate('jwt', { session: false }), ordersController.updateToDespatched);
     app.put('/api/orders/updateToOnTheWay', passport.authenticate('jwt', { session: false }), ordersController.updateToOnTheWay);
     app.put('/api/orders/updateToDelivered', passport.authenticate('jwt', { session: false }), ordersController.updateToDelivered);
