d6857a54e49c6b5c849a8124058db8fd634e0343
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-06-02 07:48:00 -0700

    Update usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -35,6 +35,6 @@ module.exports = (app, upload) => {
     app.put('/api/users/updatePoints/:id/:puntos', UsersController.updatePoints);
 
     //eliminacion de datos para
-    app.delete('/api/users/deleteAccout/:id', UsersController.deleteAccout);
+    app.delete('/api/users/deleteAccout/:idUser', UsersController.deleteAccout);
 }   
 
