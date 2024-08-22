b4fd6e288e9f3460fc054e3c97422519a611cce6
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-08-21 18:52:19 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -409,7 +409,7 @@ User.create = (user) => {
 	            created_at,
 	            updated_at
 	        )
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id)		
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8)	
     `;
 
     return db.oneOrNone(sql, [
