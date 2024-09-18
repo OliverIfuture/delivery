d1454d1947e7a95ab31e37b5bf645e8575546520
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-09-18 15:26:20 -0700

    Update usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -43,6 +43,8 @@ module.exports = (app, upload) => {
      app.post('/api/users/create_dealer', UsersController.register_dealer);
      app.post('/api/users/login_dealer', UsersController.login_dealer);
      app.put('/api/users/updateNotificationToken_dealer', UsersController.updateNotificationToken_dealer);    
+     app.get('/api/users/selectToken_dealer/:id',UsersController.selectToken_dealer);
+
 
 }   
 
