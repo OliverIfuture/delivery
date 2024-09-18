3b82ff1498786222159559aeec6a8f6dc4a50d9c
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-09-18 16:58:28 -0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -22,4 +22,18 @@ module.exports = function(passport) {
         })
     }))
 
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        User.findById_dealer(jwt_payload.id, (err, user) => {
+          if (err) {
+              return done(err, false);
+          } 
+          if (user) {
+              return done(null, user);
+          }
+          else {
+              return done(null, false);
+          } 
+        })
+    }))
+
 }
