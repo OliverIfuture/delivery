501c34e434b991db71b8b48a1864033f29a46e41
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-09-18 17:38:28 -0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,21 +4,41 @@ const User = require('../models/user');
 const Keys = require('./keys');
 
 module.exports = function(passport) {
+    // Opciones para la estrategia de usuario
+    let optsUser = {};
+    optsUser.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+    optsUser.secretOrKey = Keys.secretOrKey;
 
-    let opts = {};
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
-    opts.secretOrKey = Keys.secretOrKey;
-    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    // Estrategia para usuarios
+    passport.use('user-jwt', new JwtStrategy(optsUser, (jwt_payload, done) => {
         User.findById(jwt_payload.id, (err, user) => {
-          if (err) {
-              return done(err, false);
-          } 
-          if (user) {
-              return done(null, user);
-          }
-          else {
-              return done(null, false);
-          } 
-        })
-    }))
-}
+            if (err) {
+                return done(err, false);
+            } 
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            } 
+        });
+    }));
+
+    // Opciones para la estrategia de dealer
+    let optsDealer = {};
+    optsDealer.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+    optsDealer.secretOrKey = Keys.secretOrKey;
+
+    // Estrategia para dealers
+    passport.use('dealer-jwt', new JwtStrategy(optsDealer, (jwt_payload, done) => {
+        User.findById_dealer(jwt_payload.id, (err, dealer) => {
+            if (err) {
+                return done(err, false);
+            } 
+            if (dealer) {
+                return done(null, dealer);
+            } else {
+                return done(null, false);
+            } 
+        });
+    }));
+};
