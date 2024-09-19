88b179b2174b97d98cf9d0d65c8a096abc33f844
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-09-18 17:20:20 -0700

    Update passport2.js

--- config/passport2.js
+++ config/passport2.js
@@ -3,22 +3,22 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
 const Keys = require('./keys');
 
-module.exports = function(passport2) {
+module.exports = function(passport2) { // Cambiado a 'passport2'
 
     let opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
     opts.secretOrKey = Keys.secretOrKey;
-    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        User.findById_dealer(jwt_payload.id, (err, user) => {
-          if (err) {
-              return done(err, false);
-          } 
-          if (user) {
-              return done(null, user);
-          }
-          else {
-              return done(null, false);
-          } 
-        })     
-    }))
+
+    passport2.use(new JwtStrategy(opts, (jwt_payload, done) => {
+        User.findById(jwt_payload.id, (err, user) => { 
+            if (err) {
+                return done(err, false);
+            } 
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            } 
+        });     
+    }));
 }
