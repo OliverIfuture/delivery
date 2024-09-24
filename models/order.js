2fa0215499b6bf8562469385b70527857f26bf79
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-09-23 20:03:35 -0700

    Update order.js

--- models/order.js
+++ models/order.js
@@ -1308,7 +1308,7 @@ dealer_recharge(
     return db.oneOrNone(sql, [
     recharge.id_client,
     recharge.entity,
-      timezone('America/Mexico_City', NOW()),
+    new Date(),
     recharge.amount,
     recharge.logo,
     recharge.reference
