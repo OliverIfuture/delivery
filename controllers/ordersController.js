8d8ee40f2cf7e207f8c2e5c2b60d7de84e620cf0
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-09-23 19:23:13 -0700

    Update ordersController.js

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -824,6 +824,31 @@ module.exports = {
             });
         }
     },
+
+async createrecharge(req, res, next) {
+        try {
+
+            let recharge = req.body;
+            const data = await Order.createrecharge(recharge);
+
+            return res.status(201).json({
+
+                success: true,
+                message: 'La recarga se creo correctamente',
+                data: data.id
+            });
+
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+
+                success: false,
+                message: 'Hubo un error creando la recarga',
+                error: error
+            });
+        }
+    },
+    
     
 }
 
