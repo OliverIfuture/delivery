1796cb0c0254f895d962d1831e408db0e5bf6dbc
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-09-27 17:45:26 -0700

    Update orders_delivery_socket.js

--- sockets/orders_delivery_socket.js
+++ sockets/orders_delivery_socket.js
@@ -10,6 +10,11 @@ module.exports = (io) => {
             orderDeliveryNamespace.emit(`position/${data.id_order}`, { lat: data.lat, lng: data.lng  });
         });
 
+        socket.on('dealer_on', function(data) {
+            console.log(`EMITIO ${JSON.stringify(data)}`);
+           // orderDeliveryNamespace.emit(`position/${data.id_order}`, { lat: data.lat, lng: data.lng  });
+        });
+
         socket.on('disconnect', function(data) {
             console.log('USUARIO DESCONECTADO');
         });
