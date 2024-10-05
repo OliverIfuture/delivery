d9c24eecf0f23b3f70df99de75806bfc381f16d0
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2024-10-04 19:46:27 -0700

    Update orders_delivery_socket.js

--- sockets/orders_delivery_socket.js
+++ sockets/orders_delivery_socket.js
@@ -1,46 +1,31 @@
 module.exports = (io) => {
+  const orderDeliveryNamespace = io.of('/orders/delivery');
 
-    const orderDeliveryNamespace = io.of('/orders/delivery');
-    orderDeliveryNamespace.on('connection', function(socket) {
+  orderDeliveryNamespace.on('connection', function(socket) {
+    console.log('USUARIO CONECTADO AL NAMESPACE /orders/delivery');
+    socket.emit('welcome', { message: 'Bienvenido!' });
 
-        console.log('USUARIO CONECTADO AL NAMESPACE /orders/delivery');
-
-        socket.emit('welcome', { message: 'Bienvenido!' });
-
-
-        
-        socket.on('position', function(data) {
-            console.log(`EMITIO ${JSON.stringify(data)}`);
-            orderDeliveryNamespace.emit(`position/${data.id_order}`, { lat: data.lat, lng: data.lng  });
-        });
-
-
-        
-        socket.on('dealer_on', function(data) {
-          console.log(`machine dealer ON ${JSON.stringify(data)}`);
-          setTimeout(() => {
-            orderDeliveryNamespace.emit(`dealer_on/${data.machine}`, { 
-              id_product: data.id_product, 
-              msg: data.msg 
-            });
-          }, 3000); // Retraso de 3 segundos (3000 milisegundos)
-        });
-
-        socket.on('dealer_off', function(data) {
-          console.log(`machine dealer OFF ${JSON.stringify(data)}`);
-          setTimeout(() => {
-            orderDeliveryNamespace.emit(`dealer_off/${data.machine}`, { 
-              id_product: data.id_product, 
-              msg: data.msg 
-            });
-          }, 3000); // Retraso de 3 segundos (3000 milisegundos)
-        });
-
-
-
-        socket.on('disconnect', function(data) {
-            console.log('USUARIO DESCONECTADO');
-        });
+    socket.on('position', function(data) {
+      console.log(`EMITIO ${JSON.stringify(data)}`);
+      orderDeliveryNamespace.emit(`position/${data.id_order}`, { lat: data.lat, lng: data.lng });
     });
 
+    socket.on('dealer_on', function(data) {
+      console.log(`machine dealer ON ${JSON.stringify(data)}`);
+      setTimeout(() => {
+        orderDeliveryNamespace.emit(`dealer_on/${data.machine}`, { id_product: data.id_product, msg: data.msg });
+      }, 3000); // Retraso de 3 segundos (3000 milisegundos)
+    });
+
+    socket.on('dealer_off', function(data) {
+      console.log(`machine dealer OFF ${JSON.stringify(data)}`);
+      setTimeout(() => {
+        orderDeliveryNamespace.emit(`dealer_off/${data.machine}`, { id_product: data.id_product, msg: data.msg });
+      }, 3000); // Retraso de 3 segundos (3000 milisegundos)
+    });
+
+    socket.on('disconnect', function(data) {
+      console.log('USUARIO DESCONECTADO');
+    });
+  });
 }
