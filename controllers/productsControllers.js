01dd900d20aab62a92d5b0a4701c33d3e58bceec
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-02-06 18:48:53 -0800

    Update productsControllers.js

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -2397,4 +2397,68 @@ async createLikeProduct (req, res, next) {
         }
     },       
 
+
+
+     async createProductDealer(req, res, next) {
+        
+        let product = JSON.parse(req.body.product);
+        console.log(`Producto: ${JSON.stringify(product)}`);
+
+        const files = req.files;
+
+        let inserts = 0;
+
+        if (files.length === 0) {
+            return res.status(501).json({
+                message: 'Error al registrar el producto no tiene imagen',
+                success: false
+            });
+        }
+
+        else if (files.length > 0 && files.length <= 1 ){
+            try {
+
+                const data = await Product.createProductDealer(product);//ALMACENANDO VARIOS PRODUCTOS
+                product.id = data.id;
+
+                const start = async () => {
+                    await asyncForEach(files, async (file) => {
+                        const pathImage = `image_${Date.now()}`;
+                        const url = await storage(file, pathImage);
+
+                        if (url !== undefined && url !== null) {
+                            if (inserts == 0) {//imagen1
+                                product.image1 = url;
+
+                            }
+                        }
+
+                        await Product.update(product);
+                        inserts = inserts + 1;
+                        if (inserts == files.length) {
+                            return res.status(201).json({
+                                success: true,
+                                message: 'Producto registrado correctamente'
+
+                            });
+                        }
+                    });
+                }
+
+                start();
+
+                
+            } catch (error) {
+                console.log(`Error: ${error}`);
+                return res.status(500).json({
+                message: `Error al registrar el producto ${error}`,
+                success: false,
+                error:error
+                
+                });
+            }
+
+        }
+    },
+
 }
