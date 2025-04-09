fe35d57d2af609ef839d8f358b04b63f57d7f8b2
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-04-09 14:10:06 -0700

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -60,7 +60,7 @@ module.exports = (app, upload) => {
       app.get('/api/products/findLikes/:id_plate', productsControllers.findLikes);    
       app.get('/api/products/findLikesComent/:id_post', productsControllers.findLikesComent);    
 
-      app.get('/api/products/getAllStocks/:id_company',passport.authenticate('jwt', {session: false}) , productsControllers.getAllStocks);
+      app.get('/api/products/getAllStocks/:id_company/:id_company_product',passport.authenticate('jwt', {session: false}) , productsControllers.getAllStocks);
       app.get('/api/products/findMyProduct/:name',passport.authenticate('jwt', {session: false}) , productsControllers.findMyProduct);
       app.get('/api/products/getAllCompany',passport.authenticate('jwt', {session: false}) , productsControllers.getAllCompany);     
       app.get('/api/products/getGift',passport.authenticate('jwt', {session: false}) , productsControllers.getGift);     
