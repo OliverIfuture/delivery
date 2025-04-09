08b59159c02d18cf3c1a88fcd77f93b8ce874433
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-04-09 15:24:11 -0700

    Update category.js

--- models/category.js
+++ models/category.js
@@ -2,20 +2,21 @@ const db = require('../config/config');
 
 const Category = {};
 
-Category.getAll = () => {
+Category.getAll = (id_category_company) => {
     const sql =
         `
  SELECT 
             id,
             name,
             description,
-            image
+            image, 
+            id_category_company
         FROM
-            categories where name != 'SERVINGS' and id != 24
+            categories where name != 'SERVINGS' and id != 24 and id_category_company = $1
         ORDER BY 
             name 
         `;
-    return db.manyOrNone(sql);
+    return db.manyOrNone(sql, id_category_company);
 }
 
 Category.getAllPlates = () => {
