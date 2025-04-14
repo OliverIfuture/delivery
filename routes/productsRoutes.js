6f7c92d88965c7b7b9b200717715cce27bfc6d14
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-04-14 13:06:05 -0700

    Update productsRoutes.js

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -104,7 +104,8 @@ module.exports = (app, upload) => {
       app.get('/api/products/getByCtaegoryAndProductNameSearch/:product_name', productsControllers.getByCtaegoryAndProductNameSearch);
 
        app.get('/api/products/getByCtaegoryAndProductNamePlate/:id_category/:product_name', passport.authenticate('jwt', {session: false}), productsControllers.getByCtaegoryAndProductNamePlate);
-      app.get('/api/products/findByCategoryAndProductNameStocks/:id_category/:product_name/:id_company', passport.authenticate('jwt', {session: false}), productsControllers.findByCategoryAndProductNameStocks);
+       app.get('/api/products/findByCategoryAndProductNameStocks/:id_category/:product_name/:id_company', passport.authenticate('jwt', {session: false}), productsControllers.findByCategoryAndProductNameStocks);
+       app.get('/api/products/selectColors/:idSucursal', passport.authenticate('jwt', {session: false}), productsControllers.selectColors );
 
       app.put('/api/products/updateStock', productsControllers.updateProduct);
       app.put('/api/products/update', productsControllers.upateProduct);
