7e0392b3013d73780a8a59b1f056194e26cf8515
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-04-15 13:32:49 -0700

    Update category.js

--- models/category.js
+++ models/category.js
@@ -12,7 +12,7 @@ Category.getAll = (id_category_company) => {
             image, 
             id_category_company
         FROM
-            categories where name != 'SERVINGS' and id != 24 and id_category_company = $1
+            categories where name != 'SERVINGS' and id != 24 and id != 124 and id != 125 and id_category_company = $1
         ORDER BY 
             name 
         `;
