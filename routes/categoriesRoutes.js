9fee871e53c57c028b82fbb8b878f0226e42db6d
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-10-10 18:33:01 -0700

    Update categoriesRoutes.js

--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -7,6 +7,8 @@ module.exports = (app) => {
     *GET ROUTES
    */
     app.get('/api/categories/getAll/:id_category_company?', categoriesController.getAll);
+    app.get('/api/categories/getAllByStore/:id_category_company', categoriesController.getAllByStore);
+
     app.get('/api/categories/getAllPlates', passport.authenticate('jwt', { session: false }), categoriesController.getAllPlates);
 
     /* 
