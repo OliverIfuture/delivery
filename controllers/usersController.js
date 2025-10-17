29b845d14e875c756b340e18d0430207351926bd
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-10-17 16:48:23 -0700

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1179,4 +1179,24 @@ async createWithImageUserAndCompany(req, res, next) {
             });
         }
     },
+
+
+            async getMembershipPlan(req, res, next) {
+        try {
+            const data = await User.getMembershipPlan();
+            return res.status(201).json(data);
+
+
+        }
+        catch (error) {
+            
+            console.log(`error: ${error}`);
+            return res.status(501).json({
+                succes: false,
+                message: 'error al obtener'
+            });
+        }
+    },
+    
+    
 };
