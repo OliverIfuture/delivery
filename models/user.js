2f17dec96367327ae21f18f696d34ff9d4345e93
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-10-20 16:21:36 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -490,6 +490,40 @@ User.create = (user) => {
     ]);
 }
 
+User.create = (user) => {
+
+    const myPasswordHashed = crypto.createHash('md5').update(user.password).digest('hex');
+    user.password = myPasswordHashed;
+
+    const sql = `
+	    INSERT INTO
+	        users(
+	            email,
+	            name,
+	            lastname,
+	            phone,
+	            image,
+	            password,
+	            created_at,
+	            updated_at,
+				mi_store
+	        )
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id
+    `;
+
+    return db.oneOrNone(sql, [
+        user.email,
+        user.name,
+        user.lastname,
+        user.phone,
+        user.image,
+        user.password,
+        new Date(),
+        new Date(),
+		user.mi_store
+    ]);
+}
+
 User.update = (user) => {
     const sql = `
     UPDATE
