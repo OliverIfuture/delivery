c7e5a601e2649db57ac8b89d3488fba886963beb
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-11-04 15:16:07 -0800

    Update exercise.js

--- models/exercise.js
+++ models/exercise.js
@@ -24,10 +24,10 @@ Exercise.create = (exercise) => {
         exercise.id_company, // <-- CORREGIDO
         exercise.name,
         exercise.description,
-        exercise.muscleGroup,
+        exercise.muscle_group,
         exercise.equipment,
-        exercise.mediaUrl,
-        exercise.mediaType,
+        exercise.media_url,
+        exercise.media_type,
         new Date(),
         new Date()
     ]);
@@ -75,10 +75,10 @@ Exercise.update = (exercise) => {
         exercise.id,
         exercise.name,
         exercise.description,
-        exercise.muscleGroup,
+        exercise.muscle_group,
         exercise.equipment,
-        exercise.mediaUrl,
-        exercise.mediaType,
+        exercise.media_url,
+        exercise.media_type,
         new Date()
     ]);
 };
