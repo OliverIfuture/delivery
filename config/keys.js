e707ecade337e4f79c31dc257ac6b30f2c791188
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-11-05 19:12:18 -0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,9 @@
 module.exports = {
-    secretOrKey: '+KbPeShVkYp3s6v9y$B&E)H@McQfTjWn'
+    secretOrKey: '+KbPeShVkYp3s6v9y$B&E)H@McQfTjWn',
+    // Clave Secreta de tu cuenta de Admin de Stripe
+    stripeAdminSecretKey: process.env.STRIPE_ADMIN_SECRET_KEY, 
+    
+    // **ESTA ES LA LÍNEA QUE FALTA O ES INCORRECTA:**
+    // Lee la variable de Heroku y la exporta como 'stripeWebhookSecret'
+    stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET
 }
