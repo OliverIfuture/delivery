a4efba7e15efc0790c138863fcb37b2bb58cd0dd
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-11-11 17:31:27 -0800

    Update order.js

--- models/order.js
+++ models/order.js
@@ -615,13 +615,14 @@ Order.create = (order) => {
 	        id_order_company,
 	        lat,
 	 	    lng,
-	        affiliate_referral_id
+	        affiliate_referral_id,
+			total
 
 
 
 
         )
-    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,$13, $14, $15, $16, $17) RETURNING id
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12,$13, $14, $15, $16, $17, $18) RETURNING id
     `;
 
     return db.oneOrNone(sql, [
@@ -641,7 +642,8 @@ Order.create = (order) => {
 	    order.id_order_company,
 		order.lat,
 		order.lng,
-        order.affiliate_referral_id 
+        order.affiliate_referral_id,
+		order.total
 
     ]);
 }
