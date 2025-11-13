29dd0b4fcccd2d82287ced136efe99e8cb0ce08a
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-11-13 15:08:13 -0800

    Update posController.js

--- controllers/posController.js
+++ controllers/posController.js
@@ -151,7 +151,7 @@ module.exports = {
 
     async getSalesByShift(req, res, next) {
         try {
-            const id_shift = req.query.id_shift;
+            const id_shift = req.params.id_shift;
             console.log(`el id_shift: ${id_shift}`);
             if (!id_shift) return res.status(400).json({ success: false, message: 'Falta id_shift' });
             
