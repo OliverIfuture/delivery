55d74036c7adffa65f84ffbd3de32ea2aaefc455
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-11-13 12:38:17 -0800

    Update gym.js

--- models/gym.js
+++ models/gym.js
@@ -167,7 +167,7 @@ Gym.findPlanByName = (plan_name, id_company) => {
  */
 Gym.findMembershipById = (id_membership) => {
     const sql = `
-        SELECT id, id_client, end_date 
+        SELECT id, id_client, end_date, id_compay
         FROM gym_memberships
         WHERE id = $1
     `;
