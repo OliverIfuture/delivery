dd0b9f052aa1f2516f7bba3ea5474aa1f1ac9732
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-11-24 18:02:40 -0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1053,7 +1053,37 @@ User.getCompanyByUser = (id) => {
 User.getCompanyById = (id) => {
 
     const sql = `
-    select * from company as c
+    select 
+	c.id,
+	c.name,
+	c.addres,
+	c.telephone,
+	c.user_id,
+	c.state,
+	c.available,
+	c.type,
+	c.lat,
+	c.lng,
+	c.wantsappointments,
+	c.cashaccept,
+	c.creditcardaccepted,
+	c."stripeSecretKey",
+	c."stripePublishableKey",
+	c.membership_plan,
+	c.membership_expires_at,
+	c.membership_status,
+	c.pos,
+	c.code,
+	c.points,
+	c."stripeAccountId",
+	c."chargesEnabled",
+	c.updated_at,
+	c."acceptsAffiliates",
+	c."affiliateCommissionRate",
+	c.image_card,
+	U.notification_token
+		
+	from company as c
 	
 		inner join users as U on U.id = c.user_id
 	where c.id = $1
