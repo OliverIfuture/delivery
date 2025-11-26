5345fbaadc5cd67408d7507de75ec18d4863d37d
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-11-26 15:07:07 -0800

    Update productsControllers.js

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1856,7 +1856,7 @@ async findMyProduct(req, res, next) {
        async getAllCompany(req, res, next) {
         try {
             const data = await Product.getAllCompany();
-            console.log(`Datos enviados del usuario: ${JSON.stringify(data)}`);
+          //  console.log(`Datos enviados del usuario: ${JSON.stringify(data)}`);
             return res.status(201).json(data);
 
 
