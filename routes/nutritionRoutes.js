801ca82f667120a3899736a8e5aacd248f81149b
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-12-01 16:59:31 -0800

    Update nutritionRoutes.js

--- routes/nutritionRoutes.js
+++ routes/nutritionRoutes.js
@@ -4,4 +4,12 @@ const passport = require('passport');
 module.exports = (app) => {
     app.post('/api/nutrition/log', passport.authenticate('jwt', {session: false}), nutritionController.logFood);
     app.get('/api/nutrition/today/:id_client', passport.authenticate('jwt', {session: false}), nutritionController.getDailyLog);
+
+        // --- NUEVAS RUTAS ---
+    // Metas
+    app.post('/api/nutrition/goals', passport.authenticate('jwt', {session: false}), nutritionController.setGoals);
+    app.get('/api/nutrition/goals/:id_client', passport.authenticate('jwt', {session: false}), nutritionController.getGoals);
+    
+    // Borrar Log
+    app.delete('/api/nutrition/log/:id', passport.authenticate('jwt', {session: false}), nutritionController.deleteLog);
 }
