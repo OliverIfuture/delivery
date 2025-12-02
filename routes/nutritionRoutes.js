5ca3654a3004c842510a54f6d3e70358674c1772
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2025-12-01 20:03:00 -0800

    Update nutritionRoutes.js

--- routes/nutritionRoutes.js
+++ routes/nutritionRoutes.js
@@ -1,7 +1,7 @@
 const nutritionController = require('../controllers/nutritionController');
 const passport = require('passport');
 
-module.exports = (app) => {
+module.exports = (app,upload) => {
     app.post('/api/nutrition/log', passport.authenticate('jwt', {session: false}), nutritionController.logFood);
     app.get('/api/nutrition/today/:id_client', passport.authenticate('jwt', {session: false}), nutritionController.getDailyLog);
 
