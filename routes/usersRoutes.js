33dda3e8c2bef464a00988b70a0445e9d655804e
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2026-01-19 12:28:31 -0800

    Update usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -59,6 +59,7 @@ module.exports = (app, upload) => {
      app.get('/api/users/findClientdealer/:name',passport.authenticate('dealer-jwt', {session: false}) ,UsersController.findClientDealer);
      app.get('/api/users/getAdminsNotificationTokensDealer', passport.authenticate('dealer-jwt', {session: false}), UsersController.getAdminsNotificationTokensDealer);
      app.get('/api/users/getCompanyById/:id',passport.authenticate('jwt', {session: false}) ,UsersController.getCompanyById);
+     app.get('/api/users/getClients/:id',passport.authenticate('jwt', {session: false}) ,UsersController.getClients);
      app.get('/api/users/getAllCompanies', passport.authenticate('jwt', {session: false}), UsersController.getAllCompanies);
      app.get('/api/users/getMembershipPlan', passport.authenticate('jwt', {session: false}), UsersController.getMembershipPlan);
      app.put('/api/users/renewMembership', UsersController.renewMembership);
