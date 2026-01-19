db995223922cbfcfaaff816cb3e27c3d485c2f6a
OliverIfuture <127462579+OliverIfuture@users.noreply.github.com>
2026-01-19 12:28:59 -0800

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1360,7 +1360,26 @@ module.exports = {
         }
     },
 
+    async getClients(req, res, next) {
+        try {
+            const id = req.params.id;
 
+            const data = await User.getClients(id);
+           // console.log(`Datos enviados del getClients: ${JSON.stringify(data)}`);
+
+            if (!data) {
+                return res.status(404).json({ success: false, message: 'company no encontrado' });
+            }
+
+            return res.status(200).json(data); // Cambiar a 200 para respuesta exitosa
+        } catch (error) {
+            console.error(`error: ${error}`); // Usar console.error para errores
+            return res.status(500).json({
+                success: false,
+                message: 'Error al obtener el company por ID'
+            });
+        }
+    },
     async createWithImageUserAndCompany(req, res, next) {
         try {
             // 1. Parsear los datos JSON
