f0fd147ada788cda94272658eafd9e44b5f62a61
oliver <oliverjdm2@gmail.com>
2026-02-10 15:04:48 -0800

    f

--- models/diet.js
+++ models/diet.js
@@ -172,7 +172,7 @@ Diet.updateError = (id) => {
 
 // 4. Consultar Estado (Polling)
 Diet.findById = (id) => {
-    const sql = `SELECT * FROM ai_generated_diets WHERE id = $1`;
+    const sql = `SELECT * FROM diets WHERE id = $1`;
     return db.oneOrNone(sql, id);
 };
 
